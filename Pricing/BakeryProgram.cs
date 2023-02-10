6bd547e4b9f33cdb3219aa2c47b60b4fd8e3337b
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-10 10:35:21 -0800

    add basic project setup in Business and UI Logic files

---
+++ Pricing/BakeryProgram.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Pricer.Models;
+
+namespace Pricing
+{
+ public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
---
+++ Pricing/Models/Pricer.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+// Business Logic Goes Here
+// There should be two classes: one for Bread and one for Pastry.
+
+namespace Pricer.Models
+{
+ 
+}
