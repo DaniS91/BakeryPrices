2e2db3a95f6cd9095f1c65ce4f11f0a6e625f1a6
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-10 10:39:29 -0800

    add basic testing setup in test file

---
+++ Pricing.Tests/ModelTests/Pricer.Tests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using Pricer.Models;
+
+namespace Pricer.TestTools
+{
+  [TestClass]
+  public class PriceTests
+  {
+    [TestMethod]
+    
+  }
+}
