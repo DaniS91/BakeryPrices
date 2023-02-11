cb235ea3283159589c0960a8b3c2692e8039d554
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-11 11:32:28 -0800

    add welcome message and menu to program file

--- Pricing/BakeryProgram.cs
+++ Pricing/BakeryProgram.cs
@@ -8,7 +8,15 @@ namespace Pricing
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to...");
+      Console.WriteLine(".____ ___ _____ ____  ____  _____ ____    ____    _    _  _______ ______   __");
+      Console.WriteLine("|  _ \\_ _| ____|  _ \\|  _ \\| ____/ ___|  | __ )  / \\  | |/ / ____|  _ \\ \\ / /");
+      Console.WriteLine("| |_) | ||  _| | |_) | |_) |  _| \\___ \\  |  _ \\ / _ \\ | ' /|  _| | |_) \\ V / ");
+      Console.WriteLine("|  __/| || |___|  _ <|  _ <| |___ ___) | | |_) / ___ \\| . \\| |___|  _ < | |  ");
+      Console.WriteLine("|_|  |___|_____|_| \\_\\_| \\_\\_____|____/  |____/_/   \\_\\_|\\_\\_____|_| \\_\\|_|  ");
+      Console.WriteLine("Bread of the Day ($5 per loaf): Sourdough Round Loafs");
+      Console.WriteLine("Pastry of the Day ($2 per item): Guava & Cream Cheese Danish");
+      Console.WriteLine("Bread Deal: Buy 2 get 1 free || Pastry Deal: Buy 3 get 1 free");
     }
   }
 }
