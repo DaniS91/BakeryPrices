aa93b9da67ddd41c6e8e90e004ae320f9bdc612d
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-11 10:49:50 -0800

    create Pastry class and pastry unit price property

--- Pricing/Models/Pricer.cs
+++ Pricing/Models/Pricer.cs
@@ -7,12 +7,21 @@ using System;
 
 namespace Pricer.Models
 {
- public class Bread
- {
-  private static int loafPrice { get; set; } = 5;
-  public static int BreadPrice(int items)
+  public class Bread
   {
-    return (items * loafPrice) - ((Convert.ToInt32(Math.Floor(items / 3.0))) * loafPrice);
+    private static int loafUnitPrice { get; set; } = 5;
+    public static int BreadPrice(int items)
+    {
+      return (items * loafUnitPrice) - ((Convert.ToInt32(Math.Floor(items / 3.0))) * loafUnitPrice);
+    }
   }
- }
+
+  public class Pastry
+  {
+    private static int pastryUnitPrice { get; set; } = 2;
+    public static int PastryPrice(int items)
+    {
+      return 0;
+    }
+  }
 }
